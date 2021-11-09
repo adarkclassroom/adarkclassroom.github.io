e428430f79647c80d54f87d6d6eb1461714b2379
adarkclassroom <69877283+adarkclassroom@users.noreply.github.com>
2021-11-09 14:32:54 -0500

    Update room.js

--- script/room.js
+++ script/room.js
@@ -1,7 +1,7 @@
 /**
  * Module that registers the simple room functionality
  */
-window.alert('sadgkjasldflaksdghlaksdf help')
+window.alert('i need help')
 var Room = {
 	// times in (minutes * seconds * milliseconds)
 	_FIRE_COOL_DELAY: 5 * 60 * 1000, // time after a stoke before the fire cools
